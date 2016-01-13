080289a42638679f1e320c325f4907bfa3d65152
kenzan8000 <kenzan8000@gmail.com>
2016-01-13 11:17:27 -0800

    fix gpsaccuracy

--- air-health/Classes/Location/AIRLocationManager.swift
+++ air-health/Classes/Location/AIRLocationManager.swift
@@ -7,7 +7,7 @@ class AIRLocationManager: NSObject {
     /// MARK: - constant
     static let IntervalToStartUpdatingLocation = 20.0 // seconds to update location
     static let DistanceToUpdateLocation: CLLocationDistance = 50.0 // distance to update location
-    static let ComfirmingCountToUpdateLocation = 3 // comfirming count to update location
+    //static let ComfirmingCountToUpdateLocation = 3 // comfirming count to update location
 
     static let ThresholdOfTimeIntervalToStop: NSTimeInterval = 300
     static let ThresholdOfDistanceToStop: CLLocationDistance = 200
@@ -19,20 +19,9 @@ class AIRLocationManager: NSObject {
     /// MARK: - property
     static let sharedInstance = AIRLocationManager()
 
-    //var follower = Follower()
     var locationManager = CLLocationManager()
-//    var lastLocation: CLLocation? {
-//        didSet {
-//            if self.lastLocation == nil { return }
-//            // notification
-//            NSNotificationCenter.defaultCenter().postNotificationName(
-//                AIRNotificationCenter.UpdateLocation,
-//                object: nil,
-//                userInfo: ["location":self.lastLocation!]
-//            )
-//        }
-//    }
-    var comfirmingCountToUpdateLastLocation = 0
+    var timer : NSTimer?
+    //var comfirmingCountToUpdateLastLocation = 0
 
 
     /// MARK: - initialization
@@ -41,12 +30,18 @@ class AIRLocationManager: NSObject {
         super.init()
 
         // location manager
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        self.locationManager.requestAlwaysAuthorization()
+        //self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         if #available(iOS 9.0, *) { self.locationManager.allowsBackgroundLocationUpdates = true }
         self.locationManager.distanceFilter = 100
         self.locationManager.pausesLocationUpdatesAutomatically = false
         self.locationManager.delegate = self
+/*
+        self.locationManager.allowDeferredLocationUpdatesUntilTraveled(
+            distance: AIRLocationManager.DistanceToUpdateLocation,
+            timeout: AIRLocationManager.IntervalToStartUpdatingLocation
+        )
+*/
     }
 
 
@@ -56,15 +51,19 @@ class AIRLocationManager: NSObject {
      * start updating location
      **/
     func startUpdatingLocation() {
-        let GPSIsOff = NSUserDefaults().boolForKey(AIRUserDefaults.GPSIsOff)
-        if GPSIsOff { return }
+        dispatch_async(dispatch_get_main_queue(), { [unowned self] () -> Void in
 
-//        self.lastLocation = nil
-        self.comfirmingCountToUpdateLastLocation = 0
-        self.locationManager.startUpdatingLocation()
+            let GPSIsOff = NSUserDefaults().boolForKey(AIRUserDefaults.GPSIsOff)
+            if GPSIsOff {
+                self.stopUpdatingLocation()
+                return
+            }
 
-//        self.follower.delegate = self
-//        self.follower.beginRouteTracking()
+            //self.comfirmingCountToUpdateLastLocation = 0
+            self.locationManager.requestAlwaysAuthorization()
+            self.locationManager.startUpdatingLocation()
+
+        })
     }
 
     /**
@@ -130,6 +129,30 @@ class AIRLocationManager: NSObject {
      * @param oldLocation CLLocation
      **/
     private func updateLocation(newLocation newLocation: CLLocation, oldLocation: CLLocation) {
+
+        dispatch_async(dispatch_get_main_queue(), { [unowned self] () -> Void in
+
+            let lastLocation = AIRLocation.fetchLast(date: NSDate())
+            let distance = (lastLocation != nil) ? newLocation.distanceFromLocation(lastLocation!) : newLocation.distanceFromLocation(oldLocation)
+
+            // first location
+            if lastLocation == nil {
+                AIRLocation.save(location: newLocation)
+                self.postPollutedNotification(location: newLocation)
+                AIRUserClient.sharedInstance.postUser(location: newLocation, completionHandler: { (json) in })
+            }
+            // updating location
+            else if distance >= AIRLocationManager.DistanceToUpdateLocation && // did move?
+                newLocation.timestamp.compare(lastLocation!.timestamp) == NSComparisonResult.OrderedDescending { // is really new?
+
+                AIRLocation.save(location: newLocation)
+                self.postPollutedNotification(location: newLocation)
+                AIRUserClient.sharedInstance.postUser(location: newLocation, completionHandler: { (json) in })
+            }
+
+        })
+
+/*
         let lastLocation = AIRLocation.fetchLast(date: NSDate())
         let distance = (lastLocation != nil) ? newLocation.distanceFromLocation(lastLocation!) : newLocation.distanceFromLocation(oldLocation)
 
@@ -142,6 +165,7 @@ class AIRLocationManager: NSObject {
         // updating location
         else if distance >= AIRLocationManager.DistanceToUpdateLocation && // did move?
             newLocation.timestamp.compare(lastLocation!.timestamp) == NSComparisonResult.OrderedDescending { // is really new?
+
             if self.comfirmingCountToUpdateLastLocation >= AIRLocationManager.ComfirmingCountToUpdateLocation {
                 AIRLocation.save(location: newLocation)
                 self.postPollutedNotification(location: newLocation)
@@ -149,38 +173,12 @@ class AIRLocationManager: NSObject {
                 self.comfirmingCountToUpdateLastLocation = 0
             }
             else { self.comfirmingCountToUpdateLastLocation += 1 }
+
         }
         else { self.comfirmingCountToUpdateLastLocation = 0 }
-
         let GPSIsOff = NSUserDefaults().boolForKey(AIRUserDefaults.GPSIsOff)
         if GPSIsOff { return }
         self.locationManager.startUpdatingLocation()
-
-/*
-        let distance = (self.lastLocation != nil) ? newLocation.distanceFromLocation(self.lastLocation!) : newLocation.distanceFromLocation(oldLocation)
-        // first location
-        if self.lastLocation == nil {
-            self.lastLocation = newLocation
-            AIRLocation.save(location: newLocation)
-        }
-        // updating location
-        else if distance >= AIRLocationManager.DistanceToUpdateLocation && // did move?
-            newLocation.timestamp.compare(self.lastLocation!.timestamp) == NSComparisonResult.OrderedDescending { // is really new?
-            if self.comfirmingCountToUpdateLastLocation >= AIRLocationManager.ComfirmingCountToUpdateLocation {
-
-                self.lastLocation = newLocation
-                AIRLocation.save(location: newLocation)
-                self.comfirmingCountToUpdateLastLocation = 0
-            }
-            else { self.comfirmingCountToUpdateLastLocation += 1 }
-        }
-        else { self.comfirmingCountToUpdateLastLocation = 0 }
-
-        let GPSIsOff = NSUserDefaults().boolForKey(AIRUserDefaults.GPSIsOff)
-        if GPSIsOff { return }
-        self.locationManager.startUpdatingLocation()
-//        self.follower.endRouteTracking()
-//        self.follower.beginRouteTracking()
 */
     }
 
@@ -199,7 +197,6 @@ class AIRLocationManager: NSObject {
                 localNotification.alertBody = "BAD AIR WARNING!\n\(name) \(time)\nRecommended you to stay indoor."
                 localNotification.soundName = UILocalNotificationDefaultSoundName
                 localNotification.timeZone = NSTimeZone.defaultTimeZone()
-                //localNotification.fireDate = NSDate(timeIntervalSinceNow: 1)
                 UIApplication.sharedApplication().presentLocalNotificationNow(localNotification)
             })
         }
@@ -249,20 +246,41 @@ class AIRLocationManager: NSObject {
 extension AIRLocationManager: CLLocationManagerDelegate {
 
     func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        self.updateLocation(newLocation: newLocation, oldLocation: oldLocation)
+
+        dispatch_async(dispatch_get_main_queue(), { [unowned self] () -> Void in
+
+            if self.timer != nil && self.timer!.valid {
+                self.timer!.invalidate()
+            }
+            self.timer = NSTimer.scheduledTimerWithTimeInterval(
+                AIRLocationManager.IntervalToStartUpdatingLocation,
+                target: self,
+                selector: Selector("startUpdatingLocation"),
+                userInfo: nil,
+                repeats: false
+            )
+
+        })
+
+/*
         dispatch_after(
             dispatch_time(DISPATCH_TIME_NOW, Int64(AIRLocationManager.IntervalToStartUpdatingLocation * Double(NSEC_PER_SEC))),
             dispatch_get_main_queue(),
-            { (void) in
+            { [unowned self] () -> Void in
                 self.updateLocation(newLocation: newLocation, oldLocation: oldLocation)
             }
         )
+*/
     }
 
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        self.startUpdatingLocation()
+/*
         let GPSIsOff = NSUserDefaults().boolForKey(AIRUserDefaults.GPSIsOff)
         if GPSIsOff { return }
-
         self.locationManager.startUpdatingLocation()
+*/
     }
 
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
@@ -271,20 +289,3 @@ extension AIRLocationManager: CLLocationManagerDelegate {
         }
     }
 }
-
-
-/*
-/// MARK: - FollowerDelegate
-extension AIRLocationManager: FollowerDelegate {
-
-    func followerDidUpdate(follower: Follower) {
-        let location = self.follower.routeLocations.lastObject as! CLLocation
-        if self.lastLocation != nil {
-            self.updateLocation(newLocation: location, oldLocation: self.lastLocation!)
-        }
-        self.lastLocation = location
-    }
-
-}
-*/
-
